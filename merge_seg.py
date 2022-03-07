025474fb58e064da3c2e2d77703d0981a80e64a7
nagaraju291990 <vpp.nagaraju1234@gmail.com>
2022-03-07 19:07:30 +0530

    update to handle #

--- merge_seg.py
+++ merge_seg.py
@@ -7,7 +7,7 @@ fp1 = open(sys.argv[1], "r", encoding='utf-8') # Open file on read mode -- input
 lines = fp1.read()#.split("\n") # Create a list containing all lines
 fp1.close() # Close file
 
-lines = re.sub(r'\"SEG\d+\"', "SEG",lines)
+lines = re.sub(r'\"#SEG\d+\"', "SEG",lines)
 lines2 = lines.split("SEG")
 #print(lines)
 out = []
